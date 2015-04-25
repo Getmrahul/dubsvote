b65890d3b0bf174363df835e13b1245ffbffc98e
Rahul M <getmrahul@gmail.com>
2015-04-26 01:11:12 +0530

    S

--- app.py
+++ app.py
@@ -8,12 +8,15 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     #rand
-    randNo1 = randint(1,255)
-    randNo2 = randint(256,510)
+    randNo1 = 0
+    randNo2 = 0
+    while randNo1 == randNo2:
+        randNo1 = randint(1,255)
+        randNo2 = randint(256,510)
     obj = db.db()
     v1 = obj.video(randNo1)
     v2 = obj.video(randNo2)
-    return render_template('index.html', url1 = v1[0], url2 = v2[0], ol1 = v1[1], ol2 = v2[1])
+    return render_template('index.html',id1 = randNo1, id2 = randNo2, url1 = v1[0], url2 = v2[0], ol1 = v1[1], ol2 = v2[1])
 
 ################flask server################
 if __name__ == "__main__":
