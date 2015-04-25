1494df8cbb7205ef1fa61d0b280027d53acb7785
Rahul M <getmrahul@gmail.com>
2015-04-26 00:33:05 +0530

    S

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import os
 from flask import Flask, render_template
+from random import randint
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def index():
+    #rand
+    randNo1 = randint(1,250)
+    randNo2 = randint(251,500)
     return render_template('index.html')
---
+++ sql.sql
@@ -0,0 +1,1 @@
+create table dubs(id bigserial primary key, url char(256),ol char(256) UNIQUE, votes int);
